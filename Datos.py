a997fa595335ff7b81fc4b9e65d5c83a34c85fa8
Andrezqore <coesyp@gmail.com>
2025-03-04 14:37:59 -0600

    Actualizando codigo para uso con ejemplo

--- Datos.py
+++ Datos.py
@@ -2,7 +2,7 @@ from dbfread import DBF
 import pandas as pd
 
 # Ruta del archivo DBF
-file_path = "ruta\\del\\arhivo"  # Cambia esto por la ruta real
+file_path = "C:\\Users\\Andres\\Desktop\\BDdbf\\bascon.dbf"  # Cambia esto por la ruta real
 
 # Leer el archivo DBF y convertirlo en un DataFrame
 dbf_table = DBF(file_path, load=True)
